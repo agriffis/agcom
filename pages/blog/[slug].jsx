215300335317707303c61c599da37404c39edc42
Aron Griffis <aron@scampersand.com>
2020-11-14 11:11:58 -0500

    add next-mdx-remote and some skeleton mdx

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -3,25 +3,34 @@ import hydrate from 'next-mdx-remote/hydrate'
 import matter from 'gray-matter'
 import fs from 'fs'
 import path from 'path'
-import Head from 'next/head'
 
 const root = process.cwd()
+const blogContent = path.join(root, 'content/blog')
 
-export default function Home({mdxSource, frontMatter}) {
+export default function BlogPost({mdxSource, frontMatter}) {
   const content = hydrate(mdxSource)
   return (
     <>
-      <Head>
-        <title>{frontMatter.title}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>{content}</main>
+      <h1>{frontMatter.title}</h1>
+      {content}
     </>
   )
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: fs
+      .readdirSync(blogContent)
+      .map(p => ({params: {slug: p.replace(/\.mdx/, '')}})),
+  }
+}
+
 export async function getStaticProps({params}) {
-  const source = fs.readFileSync(path.join(root, 'content/home.mdx'), 'utf8')
+  const source = fs.readFileSync(
+    path.join(blogContent, `${params.slug}.mdx`),
+    'utf8',
+  )
   const {data, content} = matter(source)
   const mdxSource = await renderToString(content)
   return {props: {mdxSource, frontMatter: data}}
