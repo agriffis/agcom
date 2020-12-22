68525a6d4d0a6c18575dc6df2b517e3d8da8762a
Aron Griffis <aron@scampersand.com>
2020-12-21 22:49:46 -0500

    more old redirects

--- next.config.js
+++ next.config.js
@@ -2,10 +2,29 @@ const {getSlugs} = require('./slugs')
 
 module.exports = {
   async redirects() {
-    return getSlugs().map(slug => ({
-      source: '/' + slug.replace('-', '/').replace('-', '/').replace('-', '/'),
-      destination: '/' + slug,
-      permanent: true,
-    }))
+    return getSlugs()
+      .map(slug => ({
+        source:
+          '/' + slug.replace('-', '/').replace('-', '/').replace('-', '/'),
+        destination: '/' + slug,
+        permanent: true,
+      }))
+      .concat([
+        {
+          source: '/blog/articles/2012-04-24-dynamic-virtualenvwrapper.html',
+          destination: '/2012-04-24-bashrc.virtualenvwrapper',
+          permanent: true,
+        },
+        {
+          source: '/blog/articles/2013-03-25-bashes.html',
+          destination: '/2013-03-25-bashes',
+          permanent: true,
+        },
+        {
+          source: '/blog/articles/2013-08-20-pixel.html',
+          destination: '/2013-08-20-pixel',
+          permanent: true,
+        },
+      ])
   },
 }
