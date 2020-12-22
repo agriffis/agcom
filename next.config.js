0fde4f864b0fb50f72f32f27d334a94bda5312ed
Aron Griffis <aron@scampersand.com>
2020-12-22 10:31:06 -0500

    pattern-matching redirector

--- next.config.js
+++ next.config.js
@@ -1,30 +1,26 @@
-const {getSlugs} = require('./slugs')
-
 module.exports = {
   async redirects() {
-    return getSlugs()
-      .map(slug => ({
-        source:
-          '/' + slug.replace('-', '/').replace('-', '/').replace('-', '/'),
-        destination: '/' + slug,
+    return [
+      {
+        source: '/:year(\\d{4})/:month(\\d{2})/:day(\\d{2})/:slug',
+        destination: '/:year-:month-:day-:slug',
         permanent: true,
-      }))
-      .concat([
-        {
-          source: '/blog/articles/2012-04-24-dynamic-virtualenvwrapper.html',
-          destination: '/2012-04-24-bashrc.virtualenvwrapper',
-          permanent: true,
-        },
-        {
-          source: '/blog/articles/2013-03-25-bashes.html',
-          destination: '/2013-03-25-bashes',
-          permanent: true,
-        },
-        {
-          source: '/blog/articles/2013-08-20-pixel.html',
-          destination: '/2013-08-20-pixel',
-          permanent: true,
-        },
-      ])
+      },
+      {
+        source: '/blog/articles/2012-04-24-dynamic-virtualenvwrapper.html',
+        destination: '/2012-04-24-bashrc.virtualenvwrapper',
+        permanent: true,
+      },
+      {
+        source: '/blog/articles/2013-03-25-bashes.html',
+        destination: '/2013-03-25-bashes',
+        permanent: true,
+      },
+      {
+        source: '/blog/articles/2013-08-20-pixel.html',
+        destination: '/2013-08-20-pixel',
+        permanent: true,
+      },
+    ]
   },
 }
